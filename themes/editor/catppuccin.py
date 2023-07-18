b3c609abfcc5ceac7721d02dc41173c36ac1cdcd
Todd Parsons <todd.e.parsons@googlemail.com>
2023-07-18 19:46:49 +0100

    Make catppuccin optional

--- themes/editor/catppuccin.py
+++ themes/editor/catppuccin.py
@@ -1,6 +1,9 @@
-from catppuccin.extras.pygments import FrappeStyle
-from catppuccin.extras.pygments import LatteStyle
-from catppuccin.extras.pygments import MacchiatoStyle
-from catppuccin.extras.pygments import MochaStyle
+try:
+    from catppuccin.extras.pygments import FrappeStyle
+    from catppuccin.extras.pygments import LatteStyle
+    from catppuccin.extras.pygments import MacchiatoStyle
+    from catppuccin.extras.pygments import MochaStyle
+except ModuleNotFoundError:
+    raise ModuleNotFoundError("Please install `catppuccin` package in order to use catppuccin editor themes.")
 
 __all__ = ["FrappeStyle", "LatteStyle", "MacchiatoStyle", "MochaStyle"]
